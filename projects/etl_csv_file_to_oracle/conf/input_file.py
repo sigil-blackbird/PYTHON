eddcf2e07e2dcce9e085d42a4601057b2a16d5c0
sigil-blackbird <mr_develop@outlook.com>
2026-02-17 00:01:25 -0600

    etl_csv_file_to_oracle

--- projects/etl_csv_file_to_oracle/conf/input_file.py
+++ projects/etl_csv_file_to_oracle/conf/input_file.py
@@ -1,5 +1,18 @@
 from etl_csv_file_to_oracle.conf.proj_conf import get_input_path
 
 input_file_name = 'taxi_trip_data.csv'
-input_file_path = f"{get_input_path()}/{input_file_name}"
 desired_columns = ['pick_up_time', 'drop_off_time', 'trip_distance', 'trip_fare', 'payment_method', 'cab_color', 'pickup_location', 'pickup_zone', 'dropoff_location', 'dropoff_zone']
+
+def get_input_file(input_file_name):
+    """
+    Retrieve the full file path for the input CSV file.
+
+    Constructs the full file path by joining the input directory path with a predefined
+    input file name. Path separators are normalized to forward slashes.
+
+    Returns:
+        str: The absolute path to the input CSV file with forward slashes.
+    """
+    return f"{get_input_path()}/{input_file_name}"
+
+input_file_path = get_input_file(input_file_name)
